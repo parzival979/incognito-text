cd774c53dcb61802e631e264db7337c55d4597b7
SRAVANTH CHOWDARY POTLURI <bansy7777@gmail.com>
2022-04-18 23:52:01 +0530

    HEROKU PUSHING

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'whatever123'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///myDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
