991f2c72bb0a9f70a65e1ee92c1d120bfc701e26
SRAVANTH CHOWDARY POTLURI <bansy7777@gmail.com>
2022-05-02 23:43:09 +0530

    Back End Debugging

--- app.py
+++ app.py
@@ -124,8 +124,7 @@ def go_to_room():
 def send_message_in_room(id):
     send_message_form_obj = send_message_class()
     if send_message_form_obj.validate_on_submit() and not room.query.get(id) == None:
-        print(send_message_form_obj.message.data)
-        current_message = messages(message = send_message_form_obj.message.data,room_id = id,username=current_user.id)
+        current_message = messages(message = send_message_form_obj.message.data,room_id = id,username=current_user.id,message_time = datetime.utcnow())
         db.session.add(current_message)
         db.session.commit()
         return redirect(url_for('send_message_in_room',id = id))
